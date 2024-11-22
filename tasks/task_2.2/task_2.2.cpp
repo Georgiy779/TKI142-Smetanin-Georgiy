15b58f5a230fc6749cbe8ba99bacd2c027c7d7c2
Georgiy779 <georgiy.m.s@yandex.ru>
2024-11-22 17:39:31 +0300

    Задание 2.2

---
+++ tasks/task_2.2/task_2.2.cpp
@@ -0,0 +1,37 @@
+﻿#define USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+double inputx()
+{
+	double x;
+	printf("input x.\n");
+	scanf_s("%lf", &x);
+	return x;
+}
+
+double output(double x, double a)
+{
+	double y;
+	if (x > a)
+	{
+		y = x * pow(sin(x), 2);
+	}
+	else
+	{
+		y = x * pow(cos(x), 2);
+	}
+	return y;
+}
+
+int main()
+{
+	double a = 2.5, x, y;
+	x = inputx();
+	printf("x = %lf\n", x);
+	y = output(x, a);
+	printf("y = %lf\n", y);
+	return 0;
+}
