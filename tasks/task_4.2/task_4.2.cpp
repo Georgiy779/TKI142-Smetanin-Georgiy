3279f1671c5be5aed21daecb6d2212874b7f332c
Georgiy779 <georgiy.m.s@yandex.ru>
2024-12-01 19:56:59 +0300

    Доделал заданеи 4.2

--- tasks/task_4.2/task_4.2.cpp
+++ tasks/task_4.2/task_4.2.cpp
@@ -6,9 +6,12 @@
 #include <float.h>
 #include <math.h>
 #include <locale.h>
+#include <time.h> // чтобы использовать функцию time()
 
 int* intmas(int n)
 {
+	srand(time(NULL));
+
 	int f, i;
 	int* array = NULL;
 
@@ -38,71 +41,81 @@ int* intmas(int n)
 
 int zum_sred(int* array, int n)
 {
-	int g, sred_elem_mas, min;
-	double sred = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sred = sred + array[i];
-	}
-	sred = sred / n;
-
-	//Находим средний элемент массива
-	g = array[0];
-	sred_elem_mas = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (fabs(array[i] - sred) < fabs(array[i - 1] - sred))
-		{
-			g = array[i];
-			sred_elem_mas = i;
-		}
-	}
-	printf("Средний элемент массива = %d\n", array[sred_elem_mas]);
+	int min_val, sred_elem_mas, min_index;
+	
+	sred_elem_mas = (n-1)/2; //Находим средний элемент массива
 
 	//Находим минимальный элемент массива
-	g = array[0];
-	min = 0;
+	min_val = array[0];
+	min_index = 0;
 	for (int i = 0; i < n; i++)
 	{
-		if (array[i] < g)
+		if (array[i] < min_val)
 		{
-			g = array[i];
-			min = i;
+			min_val = array[i];
+			min_index = i;
 		}
 	}
 	
-	printf("Минимальный элемент массива = %d\n", array[min]);
-
-	//Меняем местами средний и минимальный элемент массива
-	g = array[sred_elem_mas];
-	array[sred_elem_mas] = array[min];
-	array[min] = g;
-	printf("Минимальный элемент массива = %d\n", array[min]);
+	printf("Минимальный элемент массива = %d\n", array[min_index]);
 	printf("Средний элемент массива = %d\n", array[sred_elem_mas]);
 
+	//Меняем местами средний и минимальный элемент массива
+	array[sred_elem_mas] ^= array[min_index] ^= array[sred_elem_mas] ^= array[min_index];
+
 	for (int i = 0; i < n; i++)
-	{
 		printf("array[%d] = %d\n", i, array[i]);
-	}
 
 	return 0;
 }
 
-//int delit(int* array, int n)
-//{
-//	return 0;
-//}
+// удалить все элементы где есть цифра X
+int* delit(int* array, int n, int* s, char X)
+{
+	int* arrayb = NULL;
+	char* str = (char*)calloc(20, sizeof(char)); // массив символов с максемальной длиной 20
+	*s = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		int sl = snprintf(str, sizeof str, "%d", array[i]); // преобразуем элемент массива array в строку
+
+
+		//проверяем есть ли в строке символ X
+		int k = 0;
+		for (k = 0; k < sl; k++)
+		{
+			if (str[k] == X)
+				break;
+		}
+
+		//если k = sl в строке нету символа x, значит это значение надо записить в новый массив
+		if (k == sl)
+		{
+			*s = *s + 1; //увеличиваю количество элементов в новом массиве
+			arrayb = (int*)realloc(arrayb, *s * sizeof(int)); // Выделяем память на ещё один элемент в массиве
+			arrayb[*s - 1] = array[i]; // записываем элемент в новый массив
+		}
+		
+	}
+	
+	free(str);
+	return arrayb;
+}
 
 int* intmasb(int* array, int n)
 {
-	int* arrayb = 0;
+	int* arrayb = NULL;
+	arrayb = (int*)calloc(n, sizeof(int));
+
 	for (int i = 0; i < n; i++)
 	{
-		if (i + 1 % 2 == 0)
+		if ((i + 1) % 2 == 0)
 			arrayb[i] = pow(array[i], 2);
 		else
 			arrayb[i] = 2 * array[i];
 	}
+
 	return arrayb;
 }
 
@@ -111,7 +124,7 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	setlocale(LC_NUMERIC, "en-US");
 
-	int n, g; //n - количество элементов массива
+	int n, s; //n - количество элементов массива
 
 	printf("Ведите n (не чётный).\n");
 	scanf_s("%d", &n);
@@ -127,6 +140,7 @@ int main()
 	if (array != NULL)
 	{
 		//Вывод массива
+		printf("Исходный массив\n");
 		for (int i = 0; i < n; i++)
 			printf("array[%d] = %d\n", i, array[i]);
 
@@ -134,14 +148,19 @@ int main()
 		zum_sred(array, n);
 
 		//Удаление всех элементов, в записи которых есть цифра 5. 
-		//delit(array, n);
+		printf("\nУдаление всех элементов, в записи которых есть цифра 5.\n");
+		int* arraya = delit(array, n, &s, '5');
+		for (int i = 0; i < s; i++)
+			printf("arraya[%d] = %d\n", i, arraya[i]);
 
 		//Сформировать новый массив на основе существующего.
+		printf("\nСформировать новый массив на основе существующего.\n");
 		int* arrayb = intmasb(array, n);
 		for (int i = 0; i < n; i++)
 			printf("arrayb[%d] = %d\n", i, arrayb[i]);
 
 		free(array);
+		free(arraya);
 		free(arrayb);
 	}
 	else
