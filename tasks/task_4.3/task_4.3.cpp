28496e0da34b37f1fc00f91fea6bcfe98979618d
AndrewAzzheurov <monokuma.kun@yandex.ru>
2024-12-02 17:35:56 +0300

    НАчал работу над 4.3

---
+++ tasks/task_4.3/task_4.3.cpp
@@ -0,0 +1,79 @@
+﻿#define _USE_MATH_DEFINES
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <float.h>
+#include <math.h>
+#include <locale.h>
+#include <time.h>
+
+int** intmas(int n, int m)
+{
+	srand(time(NULL));
+
+	int f, i, j;
+	int** array = NULL;
+
+	printf("1 - keybord\n2 - random\n");
+	scanf_s("%d", &f);
+	printf("   -> %d\n\n", f);
+
+	if (f == 1 || f == 2)
+	{
+		array = (int**)calloc(n, sizeof(int));
+		for (int i = 0; i < n; i++)
+			array[i] = (int*)calloc(n, sizeof(int));
+	}
+	else
+		printf("Неправильно введённая операция! \n");
+
+	if (f == 1)
+	{
+		for (i = 0; i < n; i++)
+			for (j = 0; j < m; j++)
+				scanf_s("%d", &array[i][j]);
+	}
+
+	if (f == 2)
+	{
+		for (i = 0; i < n; i++)
+			for (j = 0; j < m; j++)
+				array[i][j] = rand();
+	}
+
+	return array;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	setlocale(LC_NUMERIC, "en-US");
+
+	int n, m;
+
+	printf("Ведите n.\n");
+	scanf_s("%d", &n);
+	printf("   -> %d\n\n", n);
+
+	printf("Ведите m.\n");
+	scanf_s("%d", &m);
+	printf("   -> %d\n\n", m);
+
+	int** array = intmas(n, m);
+
+	if (array != NULL)
+	{
+		//Вывод массива
+		printf("Исходный массив\n");
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				printf("array[%d][%d] = %d\n", i, j, array[i][j]);
+
+		//free(array);
+	}
+	else
+		printf("error");
+
+	return 0;
+}
